172fe8c05e6e62afda085248e04b89064fe2c57c
Diogo Pinto <diogo.alvespinto@gmail.com>
2023-08-21 18:20:04 +0100

    seeded 2 users and 4 vehicles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+draco = User.new(first_name: "Draco", last_name: "Malfoy", email: "draco@gmail.com", password: "123456")
+
+nimbus = Vehicle.new(name: "Nimbus", price_per_day: 50, location: "London", description: "Greetings, mere Muggles. Draco Malfoy here, offering you the rare opportunity to ride my Nimbus 2001, a broom of unrivaled elegance and power. Imagine yourself soaring above the common folk, basking in the envy of all. But remember, this broom is a privilege, not a right. Any mishandling, and you'll face the consequences of crossing a Malfoy. Book now.", user_id: 1)
+
+hippogriff = Vehicle.new(name: "Hippogriff", price_per_day: 150, location: "London", description: "Listen up, imbeciles. Draco Malfoy here, reluctantly letting you rent my precious hippogriff. Touch it wrong, and you'll face more than a mere sting. Return it late, and you'll wish you were facing a herd of Blast-Ended Skrewts. Think you can handle it without messing up?", user_id: 1)
+
+portkey = Vehicle.new(name: "Portkey", price_per_day: 75, location: "Cornwall", description: "Ugh, listen closely, peasants. Draco Malfoy here, offering my Portkey for those too lazy to Apparate. Need instant travel? Well, it's your lucky day, but prepare your Gringotts vaults – it's costly. This isn't some charity, it's a service for the elite. Want to skip lines and flaunt your privilege?  Just remember, you're paying for convenience, not a friendly smile.", user_id: 1)
+
+sauron = User.new(first_name: "Andre", last_name: "Sauron", phone_number: "123123123", email: "andre@gmail.com", password: "123456")
+
+nazgul = Vehicle.new(name: "Nazgûl", price_per_day: 123, location: "Copenhagen", description: "Weaklings of Middle-earth, heed the Dark Lord Sauron's proposition. The Nazgûl, instruments of my terror, are for rent. Mount these beasts, spread fear, and amplify my dominion. But if returned late, prepare for eternal enslavement beside the wretched orcs. Defy me, and your soul shall fuel my malevolence. Embrace darkness, for resistance spells torment. The choice is simple, the consequences profound. The Nazgûl await, as do the fires of eternal servitude.", user_id: 2)
+
+
+draco.save
+nimbus.save
+hippogriff.save
+portkey.save
+sauron.save
+nazgul.save
+
+
+# Silver Surfboard - Silver Surfer
+
+# Surf the stars, glide through galaxies, and embrace boundless adventure. From Porto to Sydney in under 90 minutes, Crafted for unparalleled speed, our surfboard offers a transcendent and speedy experience. Safe, awe-inspiring, and available for intrepid travelers.
+
+
+# Batmobile - Bruce Wayne
+
+# Experience Porto’s iconic thrill with the Batmobile. Navigate the cityscape in unparalleled style and power. Our Batmobiles offer cutting-edge technology, sleek design, and safety features for your journey. From nights out to secret missions, embrace the vigilante spirit.
+# The Bat - Bruce Wayne
+
+# Greetings, fellow adventurers. Bruce Wayne here, thrilled to offer you the chance to experience "The Bat," my extraordinary flying machine. Embrace the skies like never before, with cutting-edge technology and sleek design. Safety and confidentiality are paramount. Explore your hero within – whether for business or leisure.
+
+
+# Iron Man Suit - Tony Stark
+
+# Listen up, mere mortals! It's your lucky day because Tony Stark, genius, billionaire, and the man behind the Iron Man suit, is offering you the chance of a lifetime. Need to up your game? Rent the pinnacle of technology and sophistication—the Iron Man suit. Feel the power surge through your veins as you become a living legend. You want style? Check. Unrivaled firepower? Check. A boost to your ego? Double-check.
+# Picture yourself soaring through the skies, commanding respect and envy from every corner. Whether you're attending a gala, making an entrance, or just flaunting your superiority, this suit has you covered. So, if you think you can handle the sheer awesomeness that is me, Tony Stark, book now.
+
+
+# Green Goblin - Green Glider
+
+# Greetings, pathetic humans. The time has come for you to bask in the malevolent magnificence of the Iron Green Glider, courtesy of yours truly, the Green Goblin. Ride the winds of chaos with wicked delight, harnessing the power of my cutting-edge technology and sinister genius. Whether you seek to spread fear or sow the seeds of anarchy, this glider is your ticket to infamy. Dare to challenge the norms of society and embrace your inner darkness.
+
+
+# Jax Teller - Harley Davidson
+
+# Hey there, road warriors! Ready to cruise like a true rebel? I'm Jax Teller, and I've got my personal Harley Davidson up for rent. Feel the freedom of the open road on this beast. But listen up, any scratches on her, and you'll wish you never sat in that saddle. This bike's seen some things, just like me. So, treat her right or face the consequences. You've been warned. If you're still up for the ride, book now!
+
+
+# Chevrolet Corvette Stingray C3 - Jimmy Carter
+
+# Yo, it's your buddy Carter from Rush Hour, and guess what? My sweet Chevrolet Corvette Stingray C3 is up for grabs! Picture yourself cruisin' like a boss, turning heads like I turn bad guys into punchlines. This car's got more attitude than I do – and that's saying something. Just remember, treat it right, or you'll have me chasing you like I'm after bad guys in Hong Kong. And remember, don’t you ever touch a black man’s radio fool!
