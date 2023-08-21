294ce2a8a82c317e4fe5dd36f2ed61b5c317123e
Danielle Jones <daniellejones_97@yahoo.com>
2023-08-21 17:04:05 +0100

    routes, relationships, and minimal validations created

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,6 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :name, :price_per_day, :description, :location, presence: true
+  validates :name, uniqueness: true
 end
