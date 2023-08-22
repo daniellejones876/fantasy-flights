86a69e50a745af4627607dc1157e651ec1926dfd
Diogo Pinto <diogo.alvespinto@gmail.com>
2023-08-22 16:28:27 +0100

    created users show and connected that to vehicles show and vice-versa

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/uikit", to:"pages#uikit"
 
   get '/dashboard', to: 'pages#dashboard'
-
+  resources :users, only: %i[show]
   resources :vehicles do
     resources :booking
   end
