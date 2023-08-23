597d27417712cc80d2cb8f48dbc0880a5939b5dc
Dani Jones <132402563+daniellejones876@users.noreply.github.com>
2023-08-23 08:18:37 -0500

    Revert "Revert "added mapbox to index, fixing syntax for adding mapbox to show""

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,6 @@ class Vehicle < ApplicationRecord
   has_many :bookings, dependent: :destroy
   validates :name, :price_per_day, :description, :location, presence: true
   validates :name, uniqueness: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
