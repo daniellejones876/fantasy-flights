9a52dbddbd3345b13e8195b1179490874ab66108
Danielle Jones <daniellejones_97@yahoo.com>
2023-08-25 10:17:33 +0100

    everything is squished into one, but functioning. need to toggle d-none

--- app/javascript/controllers/splide_controller.js
+++ app/javascript/controllers/splide_controller.js
@@ -1,18 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
 // Connects to data-controller="splide"
 export default class extends Controller {
   static targets = ["landing"]
   connect() {
     const splide = new Splide(this.landingTarget, {
-      type: 'loop',
       autoplay: true,
-      perPage: 9,
-      pauseOnHover: true,
-      interval: 2000,
+      type: 'loop',
+      perPage: 2,
+      pauseOnHover: false,
+      // easing: string = ease,
+      // interval: 2000,
+      autoScroll: {
+        speed: .5,
+      },
     });
-
     splide.mount();
   }
 }
